61a6cf4e96c5ab8000945e71565104774abbde77
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-23 08:08:00 +0500

    Now we can delete message

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name='logout'),
     path('register/', views.registerPage, name="register"),
+    path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
+
+
 ]
